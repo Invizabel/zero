737765e39b893df235766a7bf739e7a7b7a350e9
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-09-20 20:05:57 -0500

    Add files via upload

---
+++ usb/usb.h
@@ -0,0 +1,20 @@
+#include <furi_hal_usb.h>
+#include <furi_hal_usb_cdc.h>
+
+// Send data to usb-c port
+void usb_push(uint8_t * data)
+{
+    // Configure Flipper as a usb cdc device
+    furi_hal_usb_set_config(&usb_cdc_single, NULL);
+
+    // CDC interface 0
+    furi_hal_cdc_send(0, data, sizeof(data));
+}
+
+// restore usb port
+// You must call this if you want to use usb-c without rebooting your flipper on app close
+void usb_free()
+{
+    // Release usb and restore normal usb state
+    furi_hal_usb_set_config(NULL, NULL);
+}
