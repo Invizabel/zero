021050bfd32c9f18ad9d2eed70d9553d270f0cf4
Invizabel <46697708+Invizabel@users.noreply.github.com>
2026-09-21 05:12:05 -0500

    Update usb.h

--- usb/usb.h
+++ usb/usb.h
@@ -1,14 +1,16 @@
+#include <furi.h>
 #include <furi_hal_usb.h>
 #include <furi_hal_usb_cdc.h>
 
 // Send data to usb-c port
-void usb_push(uint8_t * data)
+void usb_push(uint8_t * data, int delay)
 {
     // Configure Flipper as a usb cdc device
     furi_hal_usb_set_config(&usb_cdc_single, NULL);
 
     // CDC interface 0
     furi_hal_cdc_send(0, data, sizeof(data));
+    furi_delay_ms(delay);
 }
 
 // restore usb port
